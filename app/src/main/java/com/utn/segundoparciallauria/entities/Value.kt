8df8295ad0ebfa91b97fcf12169f6a432470085a
sw9504 <sladewilson9504@gmail.com>
2022-11-19 18:16:23 -0300

    Replace customs text with material design in other fragments

---
+++ app/src/main/java/com/utn/segundoparciallauria/entities/Value.kt
@@ -0,0 +1,10 @@
+package com.utn.segundoparciallauria.entities
+
+class Value (
+    var day : String,
+    var date : String,
+    var hour : String,
+    var temperature : Int,
+    var humidity : Int
+    ) {
+}
